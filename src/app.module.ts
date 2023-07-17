596b920157f26283c3f06acb0fa229d502b5de90
Jean Alves <jean.alves@inovan.do>
2023-07-17 11:46:52 -0300

    feat: created activities resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaService } from './database/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CoursesModule } from './courses/courses.module';
+import { ActivitiesModule } from './activities/activities.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, CoursesModule],
+  imports: [UsersModule, AuthModule, CoursesModule, ActivitiesModule],
   controllers: [],
   providers: [],
 })
